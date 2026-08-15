e9d346cd410a82816caaf7bd8ba2ebeffc37e28c
TileBoard Conversion <tileboard@local>
2026-08-15 09:53:43 +0200

    Resolve header and header item function settings

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,30 +1,36 @@
 import { memo } from 'react';
+import type React from 'react';
 import type { HeaderItemConfig } from '../config/types';
 import { useAppStore } from '../store';
-import { isHidden } from '../utils/fields';
+import { isHidden, resolveFields } from '../utils/fields';
+import { HEADER_ITEM_FIELDS } from '../utils/fields';
 import { getWeatherField, getWeatherIcon, getWeatherImageStyles } from '../utils/weather';
 import Clock from './Clock';
 import DateDisplay from './DateDisplay';
 import PhotoDate from './PhotoDate';
 
 function HeaderItem({ item, slideBg }: { item: HeaderItemConfig; slideBg?: string }) {
-  if (isHidden(item, {} as never)) return null;
+  const states = useAppStore((s) => s.entities);
+  const resolved = resolveFields(item, HEADER_ITEM_FIELDS, states, null);
+  if (isHidden(resolved, states)) return null;
 
   return (
-    <div className={'header-item -' + item.type} style={item.styles}>
-      {item.type === 'time' && <Clock />}
-      {item.type === 'date' && <DateDisplay format={item.format ?? 'EEEE, LLLL dd'} />}
-      {item.type === 'datetime' && (
+    <div className={'header-item -' + resolved.type} style={resolved.styles as React.CSSProperties}>
+      {resolved.type === 'time' && <Clock />}
+      {resolved.type === 'date' && (
+        <DateDisplay format={(resolved.format as string | undefined) ?? 'EEEE, LLLL dd'} />
+      )}
+      {resolved.type === 'datetime' && (
         <>
           <Clock />
-          <DateDisplay format={item.dateFormat ?? 'EEEE, LLLL dd'} />
+          <DateDisplay format={(resolved.dateFormat as string | undefined) ?? 'EEEE, LLLL dd'} />
         </>
       )}
-      {item.type === 'custom_html' && (
-        <div dangerouslySetInnerHTML={{ __html: item.html ?? '' }} />
+      {resolved.type === 'custom_html' && (
+        <div dangerouslySetInnerHTML={{ __html: (resolved.html as string | undefined) ?? '' }} />
       )}
-      {item.type === 'weather' && <HeaderWeather item={item} />}
-      {item.type === 'photo_date' && <PhotoDate bg={slideBg} format={item.format} />}
+      {resolved.type === 'weather' && <HeaderWeather item={item} />}
+      {resolved.type === 'photo_date' && <PhotoDate bg={slideBg} format={resolved.format as string | undefined} />}
     </div>
   );
 }
